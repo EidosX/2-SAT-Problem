b7ecfa73a6f59df3903630a9abbf29e89d4d16d1
Diego Imbert <diegoimbert@pm.me>
2021-09-15 11:30:38 +0200

    Scrap DFS

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,12 +7,6 @@ public class Main {
     GraphParser parser = new GraphParser();
     Graph<String> graph = parser.parse(new Scanner(new File("assets/formule-2-sat.txt")));
 
-    List<Integer> traversalOrder = new ArrayList<>();
-    DFS dfs = new DFS();
-    dfs.setOnFinishedVisiting(node -> traversalOrder.add(node));
-    dfs.traverse(graph);
-
     System.out.println(graph);
-    System.out.println(traversalOrder);
   }
 }
