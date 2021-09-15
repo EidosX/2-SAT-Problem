e3176676cb0334ad8e64f20f771a7fb19854cf9c
Diego Imbert <diegoimbert@pm.me>
2021-09-15 11:48:49 +0200

    Kosaraju algorithm

---
+++ src/Kosaraju.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Kosaraju {
+  public static List<List<Integer>> getStronglyConnectedComponents(Graph<?> graph) {
+    ArrayList<Integer> suffixTraversalOrder = getSuffixTraversalOrder(graph);
+
+    ArrayList<List<Integer>> scc = new ArrayList<>();
+    DFS dfs = new DFS();
+    dfs.setOnExitNode(node -> scc.get(scc.size() - 1).add(node));
+    LinkedList<Integer> visited = new LinkedList<>();
+
+    Graph<?> transposed = graph.transpose();
+
+    for (int i = suffixTraversalOrder.size() - 1; i >= 0; --i) {
+      if (visited.contains(i))
+        continue;
+
+      scc.add(new LinkedList<>());
+      dfs.traverse(transposed, i, visited);
+    }
+    return scc;
+  }
+
+  private static ArrayList<Integer> getSuffixTraversalOrder(Graph<?> graph) {
+    ArrayList<Integer> order = new ArrayList<>();
+    DFS dfs = new DFS();
+    dfs.setOnExitNode(order::add);
+
+    LinkedList<Integer> visited = new LinkedList<>();
+    for (int i = 0; i < graph.order(); ++i)
+      dfs.traverse(graph, i, visited);
+    return order;
+  }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,6 +8,9 @@ public class Main {
     GraphParser parser = new GraphParser();
     Graph<String> graph = parser.parse(new Scanner(new File("assets/formule-2-sat.txt")));
 
+    List<List<Integer>> stronglyConnectedComponents = Kosaraju.getStronglyConnectedComponents(graph);
+
     System.out.println(graph);
+    System.out.println(stronglyConnectedComponents);
   }
 }
