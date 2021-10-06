fe0dd681d3cdab8261c2069d0a194bc5891cddd2
Diego Imbert <diegoimbert@pm.me>
2021-10-06 13:59:51 +0200

    Readme + changed SAT2 to support java 11

---
+++ readme.md
@@ -0,0 +1,11 @@
+# TP 1 - SAT 2 Problem
+
+Name: Diego IMBERT
+
+## Building manually
+
+In the root directory:
+Run `javac -cp src src/Main.java` to build
+Then `java -cp src Main` to run
+
+Same for running tests by replacing Main.java and Main with Tests.java and Tests
--- src/SAT2.java
+++ src/SAT2.java
@@ -1,8 +1,10 @@
+import java.util.stream.Collectors;
+
 public class SAT2 {
   public static boolean isSatisfiable(Graph<?> graph) {
     int n = graph.order() / 2;
     return Kosaraju.getStronglyConnectedComponents(graph).stream()
-        .map(c -> c.stream().map(node -> GraphParser.unnormalizeVarName(node, n)).toList())
+        .map(c -> c.stream().map(node -> GraphParser.unnormalizeVarName(node, n)).collect(Collectors.toList()))
         .noneMatch(c -> c.stream().anyMatch(node -> c.contains(-node)));
   }
 }
